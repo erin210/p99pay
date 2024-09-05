d578de4551bac68f5c5077a6876d038c23f65db6
erin210 <yourweb210@gmail.com>
2024-09-05 14:29:34 +0800

    微調

---
+++ config.js
@@ -0,0 +1,32 @@
+const environment = {
+    production: {
+      pageBaseUrl: 'https://api-page.p99pay.com',
+      apiBaseUrl: 'https://api.p99pay.com',
+    },
+    stage: {
+      pageBaseUrl: 'https://stage-api-page.p99pay.com',
+      apiBaseUrl: 'https://stage-api.p99pay.com',
+    },
+    localhost: {
+      pageBaseUrl: 'http://127.0.0.1:5500/',
+      apiBaseUrl: 'http://127.0.0.1:5500/',
+    },
+  };
+  
+  // 根據當前的環境來選擇配置
+  const currentEnv = (function() {
+    const hostname = window.location.hostname;
+    if (hostname === 'api.p99pay.com') {
+      return environment.production;
+    } else if (hostname === 'stage-api.p99pay.com') {
+      return environment.stage;
+    } else {
+      return environment.localhost;
+    }
+  })();
+  
+  // 使用方式
+  const pageBaseUrl = currentEnv.pageBaseUrl;
+  const apiUrl = currentEnv.apiBaseUrl;
+  
+  export { pageBaseUrl, apiUrl };
